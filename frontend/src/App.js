118cbeca6922f8c417b4df81e22708b98394569d
ThalitaC <thalitaceci@gmail.com>
2024-10-02 20:29:35 -0300

    Inicia estrutura do frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importe os estilos do Bootstrap
+
+function App() {
+    return (
+        <div className="App">
+            <p>Oi, eu sou um app e esse é um botao do bootstrap:</p>
+            <button className="btn btn-primary">Meu botão Bootstrap</button>
+        </div>
+    );
+}
+
+export default App;
