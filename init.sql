0a5f352e6fa06aefddf3505e54d66907cc395104
ThalitaC <thalitaceci@gmail.com>
2024-10-02 18:46:55 -0300

    Adiciona campos corretos para o banco de dados

--- init.sql
+++ init.sql
@@ -2,9 +2,19 @@ CREATE DATABASE IF NOT EXISTS `creche-conecta`;
 
 USE `creche-conecta`;
 
-CREATE TABLE `alunos` (
-    `id` INT PRIMARY KEY AUTO_INCREMENT,
-    `nome` VARCHAR(50) NOT NULL
+CREATE TABLE IF NOT EXISTS alunos (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome_aluno VARCHAR(50) NOT NULL,
+    data_nascimento DATE NOT NULL,
+    horario_entrada TIME NOT NULL,
+    horario_saida TIME NOT NULL,
+    nome_mae VARCHAR(50) NOT NULL,
+    cpf_mae VARCHAR(20) NOT NULL,
+    nome_pai VARCHAR(50),
+    cpf_pai VARCHAR(20),
+    endereco VARCHAR(100) NOT NULL,
+    telefone VARCHAR(15) NOT NULL,
+    data_inicio DATE NOT NULL,
+    valor_mensalidade DECIMAL(10, 2) NOT NULL,
+    data_desligamento DATE
 );
-
-/* TODO: adicionar demais campos */
