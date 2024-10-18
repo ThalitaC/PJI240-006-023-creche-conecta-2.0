b953a9d0ee9289542c0d9ffc406231a0039666cf
ThalitaC <thalitaceci@gmail.com>
2024-10-18 11:41:36 -0300

    edit vercel config

--- api/index.js
+++ api/index.js
@@ -1,12 +1,19 @@
 import express from "express";
-import alunosRoutes from "./routes/alunosRoutes.js";
+import router from "./routes/alunosRoutes.js";
 import cors from "cors";
 const app = express();
-const port = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(cors());
 
-app.use("/", alunosRoutes);
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    console.log("Request: " + req.method + " " + req.url);
+    next();
+});
+
+app.use("/", router);
+
+console.log("Server initialized")
 
 export default app;
